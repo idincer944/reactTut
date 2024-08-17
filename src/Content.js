6721d0265fa70df17ea6436136df9dade45c67b0
İsmail <idincer944@gmail.com>
2024-08-17 17:30:54 +0300

    First commit

---
+++ src/Content.js
@@ -0,0 +1,32 @@
+import { React, useState } from "react";
+
+const Content = () => {
+  const [name, setName] = useState("John");
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ["John", "Jane", "Bob", "Alice"];
+    const int = Math.floor(Math.random() * 4);
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+  const handleClick2 = (name) => {
+    console.log(count);
+  };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
+    </main>
+  );
+};
+
+export default Content;
